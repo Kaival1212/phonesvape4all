e71659c8ebb42ef9a54b81bddeed152bbb196e05
Kaival <kaival225@gmail.com>
2025-03-30 23:37:10 +0100

    testing main page try2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="ED-5dPjVZJbyUAmCBlHQJGn0RpbRuoP4b34G_N9V41I" />
         <title>Phones & Vapes 4 All | New Website Coming Soon</title>
 
         <!-- SEO Meta Tags -->
